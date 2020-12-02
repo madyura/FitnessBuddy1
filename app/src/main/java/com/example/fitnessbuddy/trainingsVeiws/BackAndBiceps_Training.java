8aa5f783567d8947b95470360494ab5690e6b01c
madyura <solod0780@gmail.com>
2020-12-02 11:13:36 +0100

    some changes in trainings

--- app/src/main/java/com/example/fitnessbuddy/trainingsVeiws/BackAndBiceps_Training.java
+++ app/src/main/java/com/example/fitnessbuddy/trainingsVeiws/BackAndBiceps_Training.java
@@ -15,6 +15,6 @@ public class BackAndBiceps_Training extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_BackAndBiceps__training);
+        setContentView(R.layout.activity_back_and_biceps__training);
     }
 }
